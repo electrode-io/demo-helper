d6e777fe00d9056c827cd3395ee5286eca16eeb5
Sheng Di <SDi@walmartlabs.com>
2017-05-02 16:50:32 -0700

    add an index for wrapping everything

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const defaultConfig = require("./config/default.js");
+const productionConfig = require("./config/production.js");
+const archetype = require("./archetype/config.js");
+
+module.exports = {
+  defaultConfig,
+  productionConfig,
+  archetype
+}
