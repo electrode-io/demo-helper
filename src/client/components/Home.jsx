71d32e7e0ad7e6f28092e127f7eb8203948b4aa3
Sheng Di <SDi@walmartlabs.com>
2017-07-30 13:50:58 -0700

    Beautify component generator UI

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -3,6 +3,10 @@ import { connect } from "react-redux";
 import { IntlProvider } from "react-intl";
 
 import {<%= className %>} from "<%= packageName %>";
+import "../styles/raleway.css";
+import "../styles/skeleton.css";
+import custom from "../styles/custom.css";
+import electrodePng from "../images/electrode.png";
 
 const locale = "en";
 
@@ -10,9 +14,12 @@ export class Home extends React.Component {
   render() {
     return (
       <IntlProvider locale={locale}>
-        <div>
-          <p>Some content in same parent div as</p>
-        <<%= className %> />
+        <div className={custom.demoAppContainer}>
+          <h2>
+          Hello from {" "}
+          <a href="https://github.com/electrode-io">{"Electrode"} <img src={electrodePng} /></a>
+          </h2>
+          <<%= className %> />
       </div>
       </IntlProvider>
     );
